5036a53ba86de66e1dc47320e71a8b3e671e1240
Jaganv1998 <123948253+Jaganv1998@users.noreply.github.com>
2023-02-06 15:49:18 +0530

    It is a simple program that is addition of two numbers

---
+++ AddTwoNum.java
@@ -0,0 +1,14 @@
+public class AddTwoNum {
+	
+	public void twoNum(int a, int b)
+	{
+		System.out.println("Addition od two numbers: " + (a+b));
+	}
+	
+	public static void main(String[] args)
+	{
+		AddTwoNum a = new AddTwoNum();
+		a.twoNum(5, 60);
+	}
+
+}
