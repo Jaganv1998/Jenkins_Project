21796072f2577ec1d35c50e98aa4d4f278b68005
Jaganv1998 <123948253+Jaganv1998@users.noreply.github.com>
2023-02-07 14:59:31 +0530

    Add files via upload

---
+++ SimpleJavaProject.java
@@ -0,0 +1,13 @@
+
+
+public class SimpleJavaProject {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+		System.out.println("Hi to Everyone");
+		
+	}
+
+}
